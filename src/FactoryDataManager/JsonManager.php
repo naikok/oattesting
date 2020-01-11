f66778ce1f97a05ca7a07b41fd9ddd640b98498f
naikok <sergiogayarre@gmail.com>
2020-01-11 12:59:35 +0100

    small fix

--- src/FactoryDataManager/JsonManager.php
+++ src/FactoryDataManager/JsonManager.php
@@ -33,7 +33,9 @@ class JsonManager implements IManagerData
         return $result;
     }
 
-    /* to-do implementation in order to save into json file .*/
+    /* to-do implementation in order to save into json file, not finished yetdue to lack of time .*/
+    // example array data provided to adapt it and saved it into json file, the keys of the arrays in json are different, it is required to adapt it
+    //$data = ["Question text" => $question['Question text'], "created At" => $currentDateTime, "Choice 1"=> $question['choices'][0], "Choice" => $question['choices'][1],"Choice 3" => $question['choices'][2]];
 
     public function save(array $data) : bool
     {
